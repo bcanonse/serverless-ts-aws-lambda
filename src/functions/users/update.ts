334734c5cc0001562fe27990701ec6fc97356862
Bryan Canon <bcanonse@gmail.com>
2024-08-01 10:26:33 -0600

    [CHG]Formatt update file lambda function

--- src/functions/users/update.ts
+++ src/functions/users/update.ts
@@ -1,22 +1,23 @@
 /* eslint-disable prettier/prettier */
 import {
-    type APIGatewayEvent,
+  type APIGatewayEvent,
   type APIGatewayProxyHandler,
-  type APIGatewayProxyResult} from 'aws-lambda'
+  type APIGatewayProxyResult
+} from 'aws-lambda'
 
 import { updateUser as update } from '../../services/users.services'
-import responseObject from '../../utils/response';
+import responseObject from '../../utils/response'
 
 export const updateUsers: APIGatewayProxyHandler = async (
-    event: APIGatewayEvent
-  ): Promise<APIGatewayProxyResult> => {
-    if (typeof event.body === 'string') {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const body: any = JSON.parse(event.body)
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const { id: userId }: any = event.pathParameters
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      return await update(userId, body)
-    }
-    return responseObject(404, { message: 'Body is required' })
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  if (typeof event.body === 'string') {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const body: any = JSON.parse(event.body)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { id: userId }: any = event.pathParameters
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    return await update(userId, body)
   }
+  return responseObject(404, { message: 'Body is required' })
+}
