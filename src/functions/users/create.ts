1df442c5ba9be9420f95d59590014ac67d8cc7c5
Bryan Canon <bcanonse@gmail.com>
2024-07-31 22:50:45 -0600

    [ADD]Pre-commit hook with husky

--- src/functions/users/create.ts
+++ src/functions/users/create.ts
@@ -1,14 +1,17 @@
-/* eslint-disable prettier/prettier */
-import { type APIGatewayEvent, type APIGatewayProxyHandler, type APIGatewayProxyResult } from 'aws-lambda'
+import {
+  type APIGatewayEvent,
+  type APIGatewayProxyHandler,
+  type APIGatewayProxyResult
+} from 'aws-lambda'
 import { createUsers as createUser } from '../../services/users.services'
 import responseObject from '../../utils/response'
 export const createUsers: APIGatewayProxyHandler = async (
-    event: APIGatewayEvent
-  ): Promise<APIGatewayProxyResult> => {
-    if (typeof event.body === 'string') {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const body: any = JSON.parse(event.body)
-      return await createUser(body)
-    }
-    return responseObject(404, { message: 'Body is required' })
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  if (typeof event.body === 'string') {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const body: any = JSON.parse(event.body)
+    return await createUser(body)
   }
+  return responseObject(404, { message: 'Body is required' })
+}
